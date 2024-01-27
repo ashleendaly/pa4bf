6bfb87a1abe8e7bf3c5481616bf409e5d1fc0129
Ashleen Daly <ashddev@proton.me>
2024-01-27 13:56:19 +0000

    redis setup shit

--- src/apy/app.py
+++ src/apy/app.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
+from routers.redis import router as redis_router
 
 app = FastAPI()
+
+app.include_router(redis_router)
 
 @app.get("/apy/python")
 def hello_world():
     return {"message": "Hello World"}
+
---
+++ src/apy/config.py
@@ -0,0 +1,11 @@
+import os
+from os.path import abspath, join
+from dotenv import load_dotenv
+
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+ENV = join(ROOT_DIR, '.env')
+load_dotenv(ENV)
+
+REDIS_HOST = os.environ.get("REDIS_HOST")
+REDIS_PORT= os.environ.get("REDIS_PORT")
+REDIS_PASSWORD = os.environ.get("REDIS_PASSWORD")
