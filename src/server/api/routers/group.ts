3b3746bf71a1ac06c587f02aeb2e9b814acae43a
Petros <93052465+pkitazos@users.noreply.github.com>
2024-01-27 15:04:00 +0000

    removed old procedure calls and created new procedure

--- src/server/api/routers/group.ts
+++ src/server/api/routers/group.ts
@@ -1,13 +1,25 @@
 import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import { group, groupMembership, groupOwnership } from "~/server/db/schema";
 
 export const groupRouter = createTRPCRouter({
   create: publicProcedure
-    .input(z.object({ text: z.string() }))
-    .query(({ input }) => {
-      return {
-        greeting: `Hello ${input.text}`,
-      };
+    .input(
+      z.object({
+        userId: z.string(),
+        displayName: z.string(),
+      }),
+    )
+    .mutation(async ({ ctx, input: { userId, displayName } }) => {
+      const [newGroup] = await ctx.db
+        .insert(group)
+        .values({ displayName })
+        .returning();
+
+      const groupId = newGroup!.id;
+
+      await ctx.db.insert(groupMembership).values({ groupId, userId });
+      await ctx.db.insert(groupOwnership).values({ groupId, userId });
     }),
 });
