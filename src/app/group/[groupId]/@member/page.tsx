68783afd635fa1c655ae8db22c3bbf26a02ca5cf
Jacob Trevor <50667410+JakeTrevor@users.noreply.github.com>
2024-01-28 09:59:12 +0000

    added pictures for group picture page

--- src/app/group/[groupId]/@member/page.tsx
+++ src/app/group/[groupId]/@member/page.tsx
@@ -1,12 +1,37 @@
+import { z } from "zod";
+import Image from "next/image";
+
+import { PageWrapper } from "~/components/page-wrapper";
+import { api } from "~/trpc/server";
+
 export default async function Page({
   params: { groupId },
 }: {
   params: { groupId: string };
 }) {
+  const gid = z.coerce.number().int().parse(groupId);
+
+  const images = await api.picture.getForGroup.query({ groupId: gid });
+
   return (
-    <>
-      <div>Member - {groupId}</div>
-      <div></div>
-    </>
+    <PageWrapper className="grid h-[90dvh] place-items-center">
+      <div>
+        {!!images.length && (
+          <>
+            <div>Your Photos</div>
+            {images.map((e, i) => (
+              <Image
+                key={i}
+                src={e.picture_url}
+                width={100}
+                height={100}
+                alt=""
+              />
+            ))}
+          </>
+        )}
+        {!images.length && <div>You don&apos;t have any pictures yet</div>}
+      </div>
+    </PageWrapper>
   );
 }
